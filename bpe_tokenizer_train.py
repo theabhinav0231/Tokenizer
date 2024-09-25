89e47180250da71c533572f4300a95251e5cfc9b
Abhinav <72372484+theabhinav0231@users.noreply.github.com>
2024-09-25 21:20:04 +0530

    Update bpe_tokenizer_train.py

--- bpe_tokenizer_train.py
+++ bpe_tokenizer_train.py
@@ -1,11 +1,10 @@
 from BPE_Tokenizer import BPE_Tokenizer
 
-# Train
 tokenizer = BPE_Tokenizer()
 with open("MahaBharat.txt", "r") as f:
     text = f.read()
 
+# Train
 tokenizer.train(text, 16000)
-
 # Save
 tokenizer.save("bpe_Tokenizer")
